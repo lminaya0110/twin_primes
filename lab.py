51dc67bc9055c9d06ff687702b850865b21ec8f6
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-14 09:41:32 -0400

    added twin primes fx

--- lab.py
+++ lab.py
@@ -5,6 +5,11 @@ def is_prime(candidate):
             return False
        else:
            return True
-       
-       
-    
+
+
+def find_twin_primes(upper_bound):
+    twin_primes = []
+    for num in range(2, upper_bound):
+        second = num + 2
+        if is_prime(num) and is_prime(second):
+            twin_primes.append( (num,second) )
