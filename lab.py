09b1844a3da45b5f6dc9c3bba4d10ee9dc70f567
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-14 09:59:01 -0400

    print statement

--- lab.py
+++ lab.py
@@ -13,3 +13,11 @@ def find_twin_primes(upper_bound):
         second = num + 2
         if is_prime(num) and is_prime(second):
             twin_primes.append( (num,second) )
+    return twin_primes
+
+bound = 1_000
+twin_primes = find_twin_primes(bound)
+
+for pair in twin_primes:
+    print(f'{pair[0]} and {pair[1]} are twin primes')
+    
